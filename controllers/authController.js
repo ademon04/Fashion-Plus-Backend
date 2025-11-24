c028a5f166317b778bd90427c50b4db7e558b10b
ademon04 <ballesteroscastillo2@gmail.com>
2025-11-23 20:52:47 -0600

    1.0

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,8 @@ exports.login = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role
-      }
+      },
+        token: token  
     });
   } catch (error) {
     res.status(400).json({ error: error.message });
